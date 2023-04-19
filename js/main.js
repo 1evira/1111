f4057aad67443ff978de222e91f779b038c5226d
1evira <levira_a@mail.ru>
2023-04-19 10:41:42 +0700

    start

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// navbar toggle
+$('#nav-toggle').click(function(){
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
+
+// navbar toggle on scroll
+$(window).scroll(function(e){
+    $el = $('.navbar');
+    $el.toggleClass('affix', $(this).scrollTop() > 0); 
+}); 
+function alerted() {
+    alert ("БЛАГОДАТЬ");
+}
